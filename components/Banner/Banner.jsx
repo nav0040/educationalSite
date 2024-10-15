1e24f82bc38cedb3b9ef72b3f84f8c26c28965d1
ImNaveen <naveendev004@gmail.com>
2024-10-15 07:46:26 +0530

    header, about us completed

---
+++ components/Banner/Banner.jsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <section id='home' className='banner w-[100%] h-[100vh] overflow-hidden relative'>
+            {/* <Image src={'/images/image1.jpeg'} width={1920} height={100} className='h-[700px] w-[1728px] object-cover md:object-fill banner-img' /> */}
+            <div className="relative w-full h-[720px]">
+                <Image
+                    src="/images/image1.jpeg" 
+                    alt="Banner with Gradient"
+                    layout="fill"
+                    objectFit="cover"
+                    className="absolute inset-0 z-0"
+                />
+                <div className="absolute inset-0 z-10 banner-gradient"></div>
+         
+            </div>
+            <div className='box z-[1] max-w-[800px] absolute top-[60%] left-10 translate-y-[-50%] mx-[50px]'>
+                <h2 className='text-[54px] font-[500] tracking-[1px] leading-[70.2px] mb-[30px] text-[#fff]'>Lorem ipsum dolor sit amet</h2>
+                <p className='text-[18px] font-[400] leading-[30.6px] text-[#fff]'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis velit nulla. In hac habitasse platea dictumst. Fusce non venenatis velit. Proin luctus malesuada mauris vitae mattis. Pellentesque iaculis elit lorem, et varius ipsum dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis velit nulla. In hac habitasse platea dictumst.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Banner
--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+'use client'
 
+import React, { useState } from 'react'
+import { GiHiking } from "react-icons/gi";
+import { CiMenuFries } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
+import Link from 'next/link';
+import Image from 'next/image';
 const categories =[
     { name:"About Us",link:'#'},
     { name:'How We Help',link:"#trending"},
@@ -15,7 +21,36 @@ const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
     const [id, setId] = useState('home');
   return (
-    <div>Header</div>
+    <header
+    style={{ background: visible || showMenu ? 'var(--bg-main)':'transparent',
+        boxShadow: visible ? '2px 20px 30px var(--shadow-color)':'none'
+     }} 
+     className='w-[100%] fixed top-0 left-0 z-[100] select-none transition-all ease-in-out-[0.3s]'
+
+    >
+        <nav className='w-[100%] max-w-[--max-width] flex justify-around  items-center mx-auto py-[15px] px-[25px]'>
+            <Link href={'#'} className='logo flex items-center gap-3 text-[2.6rem] font-[700]'>
+             <Image src='/images/image2.png' width={134} height={42}/>
+            </Link>
+            <div className='menu_btn' onClick={()=> setShowMenu(value => !value)}>
+            { showMenu ? <IoMdClose /> : <CiMenuFries /> } 
+            </div>
+            <ul className='flex justify-center items-center ' style={{ height: showMenu ? 400 : 0}}>
+                {
+                  categories.map(category => (
+                    <li key={category.name}>
+                        <Link href={category.link} className={`text-[0.9rem] text-white mx-[12px] transition-all ease-[0.3s] capitalize ${id === category.name ? 'active':''}`}>
+                          {category.name}
+                        </Link>
+                         
+                    </li>
+                  ))
+                }
+            </ul>
+            <button className='hidden md:block bg-white rounded-md px-5 py-3'>Contact Us</button>
+        </nav>
+        
+    </header>
   )
 }
 
